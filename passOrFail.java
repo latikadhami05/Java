9a70c2cdc15f25481ca31314ea5cb8ae23195c54
latikadhami05 <latikadhami06@gmail.com>
2025-08-05 14:13:10 +0530

    simple code

---
+++ passOrFail.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class passOrFail {
+    public static void main(String[] args) {
+        int marks = 22;
+        String reportCard = (marks >= 33) ? "pass" : "fail";
+        System.out.println(reportCard);
+    }
+
+}

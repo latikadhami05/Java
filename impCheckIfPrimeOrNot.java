6b5c29a3207121aabd776395a357aae6c010fe19
latikadhami05 <latikadhami06@gmail.com>
2025-08-09 09:48:09 +0530

    Create impCheckIfPrimeOrNot.java

---
+++ impCheckIfPrimeOrNot.java
@@ -0,0 +1,19 @@
+//very very important 
+// asked in interviews
+
+public class impCheckIfPrimeOrNot {
+    public static boolean isPrime(int n) {
+        boolean isPrime = true;
+        for (int i = 2; i <= n - i; i++) {
+            if (n % i == 0) { // completely dividing
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(12));
+    }
+}

f09e176cdbf12fb361c06ace7dae966e6dc653bb
latikadhami05 <latikadhami06@gmail.com>
2025-08-11 07:38:00 +0530

    do while in java

---
+++ doWhile.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class doWhile {
+    public static void main(String[] args) {
+        int counter = 1;
+        do {
+            System.out.println("NICE");
+            counter++;
+        } while (counter <= 10);
+    }
+}

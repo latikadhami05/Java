0ec6a866894135d4be44075251b658c50f24693a
latikadhami05 <latikadhami06@gmail.com>
2025-08-13 09:26:29 +0530

    If Else in Java

---
+++ ifElse.java
@@ -0,0 +1,26 @@
+
+public class ifElse {
+    // public static void main(String args[]) {
+    // int age = 16;if(age>=18)
+    // {
+    // System.out.println("Adult: drive ,vote");
+    // }if(age>13&&age<18)
+    // {
+    // System.out.println("Teenager");
+    // }else
+    // {
+    // System.out.println("Not adult");
+    // }
+    // }}
+
+    public static void main(String args[]) {
+        int A = 10;
+        int B = 5;
+
+        if (A >= B) {
+            System.out.println("A is largest among two");
+        } else {
+            System.out.println("B is largest among two");
+        }
+    }
+}

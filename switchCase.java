f6ea675eb117d16b2d9d7d118e56b69bdb20edbc
latikadhami05 <latikadhami06@gmail.com>
2025-08-16 11:28:58 +0530

    Switch Case

---
+++ switchCase.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class switchCase {
+    public static void main(String args[]) {
+        int number = 2;
+        switch (number) {
+            case 1:
+                System.out.println("samosa");
+                break;
+            case 2:
+                System.out.println("cake");
+                break;
+            default:
+                System.out.println("wake up");
+
+        }
+    }
+}

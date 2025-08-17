211b0903a1d2903664eace786e01b91261f02eaf
latikadhami05 <latikadhami06@gmail.com>
2025-08-17 16:08:55 +0530

    while loop in java

---
+++ whileLoop.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println("latika");
+            counter++;
+        }
+        System.out.println("printed hieee 100*");
+    }
+}

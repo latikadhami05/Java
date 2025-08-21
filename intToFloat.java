e856adaa66da6e0ec5c5f0a0876306283a768674
latikadhami05 <latikadhami06@gmail.com>
2025-08-21 18:43:07 +0530

    int to float in java

---
+++ intToFloat.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class intToFloat {
+    public static void main(String[] args) {
+        scanner sc = new scanner(System.in);
+        float a = 25.12f;
+        int b = (int) a;
+    }
+}

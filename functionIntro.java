8e4d9663896e8bcaa9facd671d3d48a0efd5a15d
latikadhami05 <latikadhami06@gmail.com>
2025-08-24 14:18:00 +0530

    function intro in java

---
+++ functionIntro.java
@@ -0,0 +1,15 @@
+//SYNTAX OF FUNCTION:
+// returnType name(){
+//body
+//  return statement;    
+// }
+
+public class functionIntro {
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}

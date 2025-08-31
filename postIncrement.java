150381b1b226f418cc75b493c28064791177bbad
latikadhami05 <latikadhami06@gmail.com>
2025-08-31 12:12:06 +0530

    post increment in java

---
+++ postIncrement.java
@@ -0,0 +1,10 @@
+
+
+public class postIncrement {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a++; // for post decrement use a--
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

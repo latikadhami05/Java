43559c985f5ccf12eb267bf2deed2e40dadefb1c
latikadhami05 <latikadhami06@gmail.com>
2025-09-02 09:15:56 +0530

    relational Operators in java

---
+++ relationalOperators.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class relationalOperators {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 10;
+        System.out.println((a != b));
+    }
+}
+// answer will be false as 10=10

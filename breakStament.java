0ce52e1039e87c65499bec2139e517847bf82565
latikadhami05 <latikadhami06@gmail.com>
2025-09-08 08:22:10 +0530

    break statement in java

---
+++ breakStament.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class breakStament {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("i am out of the loop by break statement");
+    }
+}

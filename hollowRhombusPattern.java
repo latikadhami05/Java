37b0c949fc3df0b9e1556c6582baef759647a572
latikadhami05 <latikadhami06@gmail.com>
2025-09-14 10:24:37 +0530

    Pattern

---
+++ hollowRhombusPattern.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+public class hollowRhombusPattern {
+    public static void hollow_Rhombus_Pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+
+            // spaces
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // hollow rectangle-stars
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        hollow_Rhombus_Pattern(5);
+    }
+}

0cd00773314ab281b135bdbb0ab82ba6a230ae84
latikadhami05 <latikadhami06@gmail.com>
2025-09-16 19:45:35 +0530

    pattern

---
+++ invertedStarPattern.java
@@ -0,0 +1,12 @@
+
+public class invertedStarPattern {
+    public static void main(String[] args) {
+        int n = 18;
+        for (int line = 1; line <= n; line++) {
+            for (int star = 1; star <= n - line + 1; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

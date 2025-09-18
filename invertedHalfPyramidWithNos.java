f0302fb5d02031487fc85f6d236d877b83f1c85a
latikadhami05 <latikadhami06@gmail.com>
2025-09-18 22:21:40 +0530

    inverted half pyramind

---
+++ invertedHalfPyramidWithNos.java
@@ -0,0 +1,18 @@
+
+public class invertedHalfPyramidWithNos {
+    public static void inverted_half_pyramid_WithNumbers(int n) {
+
+        for (int i = 1; i <= n; i++) {
+
+            // inner numbers
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        inverted_half_pyramid_WithNumbers(7);
+    }
+}

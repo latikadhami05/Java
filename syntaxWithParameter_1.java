de799c566fc5aefbb426b16ce8b3e1935f9abb12
latikadhami05 <latikadhami06@gmail.com>
2025-09-24 08:30:58 +0530

    syntax with parameter

---
+++ syntaxWithParameter_1.java
@@ -0,0 +1,21 @@
+//SYNTAX:
+// returnType name(type param1,type param2){
+//   //body
+// return statement;
+// }
+
+import java.util.*;
+
+public class syntaxWithParameter_1 {
+    public static void calculateSum(int num1, int num2) {
+        int sum = num1 + num2;
+        System.out.println("sum is:" + sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        calculateSum(a, b);
+    }
+}
